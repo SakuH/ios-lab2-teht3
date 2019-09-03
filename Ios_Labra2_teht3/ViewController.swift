6915a88e2f7718dcee26e77e4aa9a5ecdca3de02
sakuh <saku.huuha@gmail.com>
2019-09-03 18:33:48 +0300

    first commit

--- Ios_Labra2_teht3/ViewController.swift
+++ Ios_Labra2_teht3/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var NameTextField: UITextField!
+    @IBOutlet var GreetingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func EnglishButton(_ sender: Any) {
+        GreetingLabel.text = "Hi " + NameTextField.text! + "!"
+    }
+    
+    @IBAction func SwedishButton(_ sender: Any) {
+        GreetingLabel.text = "Hejsan " + NameTextField.text! + "!"
+    }
+    
+    @IBAction func FinnishButton(_ sender: Any) {
+        GreetingLabel.text = "Terve " + NameTextField.text! + "!"
+    }
+    
+    @IBAction func SurpriseButton(_ sender: Any) {
+        GreetingLabel.text = "Konnichiwa " + NameTextField.text! + "!"
+    }
+    
 }
 
